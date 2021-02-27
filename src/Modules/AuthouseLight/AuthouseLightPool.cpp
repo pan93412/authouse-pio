5ec6e6f2c9ff5860f1dc084b68c2dfc9a82b091e
Yi-Jyun Pan <pan93412@gmail.com>
2021-02-28 02:52:29 +0800

    feat(AuthouseLightPool): done

--- src/Modules/AuthouseLight/AuthouseLightPool.cpp
+++ src/Modules/AuthouseLight/AuthouseLightPool.cpp
@@ -1,4 +1,71 @@
 #include "AuthouseLightPool.hpp"
+#define THIS AuthouseLightPool
 
-AuthouseLightPool* AuthouseLightPool::getInstance() {
+THIS* THIS::getInstance() {
+    if (!instance) {
+        instance = new AuthouseLightPool();
+    }
+
+    return instance;
 }
+
+THIS* THIS::add(AuthouseLight* authouseLight) {
+    lights.push_back(authouseLight);
+    return instance;
+}
+
+THIS* THIS::add(int pin) {
+    AuthouseLight* lightInstance = new AuthouseLight(pin);
+    lights.push_back(lightInstance);
+    return instance;
+}
+
+AuthouseLight** THIS::_query(int pin) {
+    for (auto i = lights.begin(); i < lights.end(); i++) {
+        if ((*i)->getPin() == pin) return i;
+    }
+    return nullptr;
+}
+
+AuthouseLight* THIS::query(int pin) {
+    AuthouseLight** lightInstance = this->_query(pin);
+    if (lightInstance) return *lightInstance;
+    return nullptr;
+}
+
+THIS* THIS::remove(int pin, bool delete_detached_instance = true) {
+    AuthouseLight** lightInstance = this->_query(pin);
+    if (lightInstance != nullptr) {
+        lights.erase(lightInstance);
+        if (*lightInstance != nullptr && delete_detached_instance) delete *lightInstance;
+    }
+
+    return this;
+}
+
+THIS* THIS::turnOnAll() {
+    for (auto i = lights.begin(); i < lights.end(); i++) {
+        (*i)->activate();
+    }
+
+    return this;
+}
+
+THIS* THIS::turnOffAll() {
+    for (auto i = lights.begin(); i < lights.end(); i++) {
+        (*i)->deactivate();
+    }
+
+    return this;
+}
+
+THIS* THIS::remove(int pin) {
+    AuthouseLight** lightInstance = this->_query(pin);
+    if (lightInstance != nullptr) {
+        lights.erase(lightInstance);
+    }
+
+    return this;
+}
+
+#undef THIS
