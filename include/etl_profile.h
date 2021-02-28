2313b156e32cb78c8e8736a89d5c26781068a6f1
Yi-Jyun Pan <pan93412@gmail.com>
2021-02-28 13:53:02 +0800

    feat(etl): add ETL_NO_STL

--- include/etl_profile.h
+++ include/etl_profile.h
@@ -1,9 +1,13 @@
 #ifndef __ETL_PROFILE_H__
 #define __ETL_PROFILE_H__
 
-#define ETL_THROW_EXCEPTIONS
 #define ETL_VERBOSE_ERRORS
 #define ETL_CHECK_PUSH_POP
+
+// Arudino itself does not support STL, we use
+// the reverse engineered version of STL from ETL.
+#define ETL_NO_STL
+
 #define ETL_TARGET_OS_FREERTOS // Placeholder. Not currently utilised in the ETL
 #define ETL_TARGET_DEVICE_ARM  // Placeholder. Not currently utilised in the ETL
 
