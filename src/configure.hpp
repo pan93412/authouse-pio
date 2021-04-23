9184de205dcce56422991909dbf0a04c7e572e11
Yi-Jyun Pan <pan93412@gmail.com>
2021-04-23 20:55:54 +0800

    feat(configure): add configurations

---
+++ src/configure.hpp
@@ -0,0 +1,29 @@
+/**
+ * Authouse.Configure
+ *   @brief Configure Authouse.
+ *   @author pan93412
+ *   @date 2021-4-23
+ */
+
+#ifndef AH_Configure
+#define AH_Configure
+
+/**
+ * Support flags.
+ * To remove one of a feature, change true to false.
+ */
+
+/// Enable Authouse Light support.
+const bool LIGHT_SUPPORT = true;
+/// Enable DHT11 sensor support.
+const bool DHT11_SUPPORT = true;
+/// Enable SHARP GP2Y1014AU0F sensor support.
+const bool SHGP2Y_SUPPORT = true;
+
+/// The serial port that DHT11 used.
+const int DHT11_PORT = 4;
+
+/// The serial port that SHARP GP2Y1014AU0F used.
+const int SHGP2Y_PORT = 2;
+
+#endif
