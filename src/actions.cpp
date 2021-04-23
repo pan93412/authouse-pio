9d9f650dc331119857ece8185d4edbf8332faeae
Yi-Jyun Pan <pan93412@gmail.com>
2021-04-23 20:23:00 +0800

    refactor(actions): make it easier to modify

--- src/actions.cpp
+++ src/actions.cpp
@@ -6,33 +6,51 @@
  */
 #include "actions.hpp"
 
-inline void enable_authouse_light(AuthouseLightPool *pool, SerialCommunication *serialCommunication) {
+#define use(name) auto name = dependencies->name;
+
+action(enable_authouse_light) {
+    use(pool)
+    use(serialCommunication)
+
     pool->turnOnAll();
     serialCommunication->postMessage("{\"light_enabled\": true}");
 }
 
-inline void disable_authouse_light(AuthouseLightPool *pool, SerialCommunication *serialCommunication) {
+action(disable_authouse_light) {
+    use(pool)
+    use(serialCommunication)
+
     pool->turnOffAll();
     serialCommunication->postMessage("{\"light_enabled\": false}");
 }
 
-inline void get_dht11_data(DHT11Temp* dht11, SerialCommunication *serialCommunication, TemperatureInfo* tempInfo) {
+action(get_dht11_data) {
+    use(dht11)
+    use(serialCommunication)
+    use(tempInfo)
+
     dht11->update();
-    serialCommunication->postMessageNoNl("{\"temperature\": ");
+    serialCommunication->postMessageNoNl("{\"temperature\": \"");
     serialCommunication->postMessageNoNl(String(tempInfo->temperature));
-    serialCommunication->postMessageNoNl(", \"humidity\": ");
+    serialCommunication->postMessageNoNl("\", \"humidity\": \"");
     serialCommunication->postMessageNoNl(String(tempInfo->humidity));
-    serialCommunication->postMessage("}");
+    serialCommunication->postMessage("\"}");
 };
 
-void test_input_output(SerialCommunication *serialCommunication) {
+action(test_input_output) {
+    use(serialCommunication)
+
     String read;
     serialCommunication->postMessage("Start receiving inputs.");
     serialCommunication->readRequest(&read);
     serialCommunication->postMessage(read);
 };
 
-void get_pm25_data(SHGP2YSensor* sensor, SerialCommunication *serialCommunication, SHGP2YSensorInfo* data) {
+action(get_pm25_data) {
+    use_custom(pm25, sensor)
+    use_custom(pm25Info, data)
+    use(serialCommunication)
+
     sensor->update();
     serialCommunication->postMessageNoNl("{\"working\": ");
     serialCommunication->postMessageNoNl(data->working ? "true" : "false");
@@ -46,16 +64,16 @@ void get_pm25_data(SHGP2YSensor* sensor, SerialCommunication *serialCommunicatio
 void action_loop(int input, Dependencies* dependencies) {
     switch (input) { // received...
         case 'e':
-            enable_authouse_light(dependencies->pool, dependencies->serialCommunication);
+            enable_authouse_light(dependencies);
             break;
         case 'd':
-            disable_authouse_light(dependencies->pool, dependencies->serialCommunication);
+            disable_authouse_light(dependencies);
             break;
         case 't':
-            get_dht11_data(dependencies->dht11, dependencies->serialCommunication, dependencies->tempInfo);
+            get_dht11_data(dependencies);
             break;
         case 'm':
-            get_pm25_data(dependencies->pm25, dependencies->serialCommunication, dependencies->pm25Info);
+            get_pm25_data(dependencies);
             break;
         case 'p':
             test_input_output(dependencies->serialCommunication);
