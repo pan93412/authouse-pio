7d5594fc816afc2255e470158248e853c8064636
Yi-Jyun Pan <pan93412@gmail.com>
2021-04-23 20:56:24 +0800

    feat(main): the enabled features is configurable

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 #include "Modules/AuthouseLight/AuthouseLightPool.hpp"
 #include "Sensor/Temperature/DHT11/DHT11.hpp"
 #include "actions.hpp"
+#include "configure.hpp"
 #include <Arduino.h>
 #include <etl_profile.h>
-
-const int DHT11_PORT = 4;
-const int SHGP2Y_PORT = 2;
 
 Dependencies *dependencies;
 
@@ -20,18 +18,24 @@ void setup() {
     dependencies->serialCommunication->initiate();
 
     // light pool
-    dependencies->pool = AuthouseLightPool::getInstance();
-    dependencies->pool->add((new AuthouseLight{LED_BUILTIN})->initiate());
+    if (LIGHT_SUPPORT) {
+        dependencies->pool = AuthouseLightPool::getInstance();
+        dependencies->pool->add((new AuthouseLight{LED_BUILTIN})->initiate());
+    }
 
     // DHT11
-    dependencies->tempInfo = new TemperatureInfo();
-    dependencies->dht11 = new DHT11Temp(DHT11_PORT, dependencies->tempInfo);
-    dependencies->dht11->begin();
+    if (DHT11_SUPPORT) {
+        dependencies->tempInfo = new TemperatureInfo();
+        dependencies->dht11 = new DHT11Temp(DHT11_PORT, dependencies->tempInfo);
+        dependencies->dht11->begin();    
+    }
 
     // PM2.5
-    dependencies->pm25Info = new SHGP2YSensorInfo();
-    dependencies->pm25 = new SHGP2YSensor(SHGP2Y_PORT, dependencies->pm25Info);
-    dependencies->pm25->begin();
+    if (SHGP2Y_SUPPORT) {
+        dependencies->pm25Info = new SHGP2YSensorInfo();
+        dependencies->pm25 = new SHGP2YSensor(SHGP2Y_PORT, dependencies->pm25Info);
+        dependencies->pm25->begin();
+    }
 }
 
 void loop() {
